e15d468ae1311105122c3e54d93b37ce93a28e73
Eric <ejp5559@psu.edu>
2023-10-23 18:57:10 -0400

    Added placeholder events on the events page

--- screens/EventList.jsx
+++ screens/EventList.jsx
@@ -1,10 +1,36 @@
-import { StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { Card } from 'react-native-elements';
 
 
 export default function EventListScreen(){
     return(
-        <View style={{justifyContent:'center', height:'100%'}}>
-            <Text style={{textAlign:'center'}}>EventList</Text>
-        </View>
+
+            <View style={{justifyContent:'center', flex:1}}>
+                <Text style={{textAlign:'center'}}>EventList</Text>
+                <ScrollView>
+                    <Card>
+                        <Card.Image source={require('../assets/my-pi-2-alt.png')}></Card.Image>
+                         <Text>I am a placeholder event!</Text>
+                    </Card>
+                    <Card>
+                        <Card.Image source={require('../assets/my-pi-2-alt.png')}></Card.Image>
+                         <Text>I am a placeholder event!</Text>
+                    </Card>
+                    <Card>
+                        <Card.Image source={require('../assets/my-pi-2-alt.png')}></Card.Image>
+                         <Text>I am a placeholder event!</Text>
+                    </Card>
+                    <Card>
+                        <Card.Image source={require('../assets/my-pi-2-alt.png')}></Card.Image>
+                         <Text>I am a placeholder event!</Text>
+                    </Card>
+                    <Card>
+                        <Card.Image source={require('../assets/my-pi-2-alt.png')}></Card.Image>
+                         <Text>I am a placeholder event!</Text>
+                    </Card>
+                </ScrollView>
+            </View>
+
+
     )
 }
