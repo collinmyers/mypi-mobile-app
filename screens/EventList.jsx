3699c447bde0d47a90f9382065a59cb193ecde80
Max Smith <max.smith.2425@gmail.com>
2023-10-31 15:04:59 -0400

    made event clickable

--- screens/EventList.jsx
+++ screens/EventList.jsx
@@ -1,18 +1,29 @@
-import React from "react";
-import { ScrollView, Text, View } from "react-native";
+import React, { useState } from "react";
+import { ScrollView, Text, View, TouchableOpacity } from "react-native";
 import { Card } from "react-native-elements";
-
+import EventDetailsModal from "./EventDetails"; 
 
 export default function EventListScreen(){
+
+    const [isModalVisible, setModalVisible] = useState(false);
+
+    const toggleModal = () => {
+      setModalVisible(!isModalVisible);
+    };
+
+
+
     return(
 
             <View style={{justifyContent:"center", flex:1}}>
                 <Text style={{textAlign:"center"}}>EventList</Text>
                 <ScrollView>
-                    <Card>
-                        <Card.Image source={require("../assets/my-pi-2-alt.png")}></Card.Image>
+                    <TouchableOpacity onPress={toggleModal}>
+                        <Card>
+                         <Card.Image source={require("../assets/my-pi-2-alt.png")}></Card.Image>
                          <Text>I am a placeholder event!</Text>
-                    </Card>
+                         </Card>
+                     </TouchableOpacity>
                     <Card>
                         <Card.Image source={require("../assets/my-pi-2-alt.png")}></Card.Image>
                          <Text>I am a placeholder event!</Text>
@@ -30,6 +41,7 @@ export default function EventListScreen(){
                          <Text>I am a placeholder event!</Text>
                     </Card>
                 </ScrollView>
+                <EventDetailsModal isVisible={isModalVisible} onClose={toggleModal} />
             </View>
 
 
