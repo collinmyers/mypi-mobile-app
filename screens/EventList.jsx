7cf04e2657efd27334316c484e51973e68376d75
ericpetika <ejp5559@psu.edu>
2023-11-03 07:59:10 -0400

    Events connected to db. Just need to render each event

--- screens/EventList.jsx
+++ screens/EventList.jsx
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { ScrollView, SafeAreaView } from "react-native";
 import { Text } from "react-native-paper";
 import { Card } from "react-native-elements";
-
+import { Databases, Client } from "appwrite";
 
 // import AppStyle from "../styling/AppStyling";
 import HomeStyle from "../styling/HomeStyling";
 
 
 export default function EventListScreen() {
+
+    const getEvents = async() =>{
+        try{
+            const client = new Client()
+                .setEndpoint(process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT)
+                .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT);
+            
+            const database = new Databases(client);
+
+            let promise = database.listDocuments(
+                "653ae4b2740b9f0a5139", //DB ID
+                "6543f2c30111d4e20bde", //Collection ID
+            );
+            promise.then(function (response){
+                console.log(response);  
+            }, function(error){
+                console.log(error);
+            });
+        }
+        catch(error){
+            console.error(error);
+        }
+    };
+
+    useEffect(()=> {
+        getEvents();
+    },[]);
+
     return (
 
         <SafeAreaView style={HomeStyle.eventContainer}>
