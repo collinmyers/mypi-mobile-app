6f31f6e35e41128da66601f06328dee5e2fc31a2
Eric <ejp5559@psu.edu>
2023-11-05 16:11:56 -0500

    Attempt to render events from db.  Not functional at moment.

--- screens/Central/EventList.jsx
+++ screens/Central/EventList.jsx
@@ -10,7 +10,10 @@ import HomeStyle from "../../styling/HomeStyling";
 
 export default function EventListScreen() {
 
+    const eventListing = [];
+
     const getEvents = async () => {
+
         try {
             const client = new Client()
                 .setEndpoint(process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT)
@@ -18,18 +21,30 @@ export default function EventListScreen() {
 
             const database = new Databases(client);
 
+            //Get docs from specified collection from db
             let promise = database.listDocuments(
                 "653ae4b2740b9f0a5139", //DB ID
                 "6543f2c30111d4e20bde", //Collection ID
             );
+
+            //Successful pull from db.  I tried doing a for loop to create a card for each
+            //event, but I think there are sync issues with the promise... not sure how to handle
+            //
             promise.then(function (response) {
                 console.log(response);
+                console.log(response["documents"][0]["Name"]);
+                eventListing.push(
+                    <Card key={0}>
+                        <Text>{response["documents"][0]["Name"]}</Text>
+                    </Card>
+                );
             }, function (error) {
-                console.log(error);
+                console.log(error); //promise failure
             });
         }
+
         catch (error) {
-            console.error(error);
+            console.error(error); //catch error
         }
     };
 
@@ -41,6 +56,15 @@ export default function EventListScreen() {
 
         <SafeAreaView style={HomeStyle.eventContainer}>
             <ScrollView showsVerticalScrollIndicator={false}>
+
+                {/* 
+                    Want to put events from db here 
+                    Should be able to just do
+                    {eventListing}
+                */}
+
+                {console.log(eventListing)}  
+
                 <Card>
                     <Card.Image source={require("../../assets/my-pi-2-alt.png")}></Card.Image>
                     <Text>I am a placeholder event!</Text>
