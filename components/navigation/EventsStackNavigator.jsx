955aa69abaa573f7d52875c3038e8d90a8468031
ericpetika <ejp5559@psu.edu>
2023-11-22 21:35:24 -0500

    Transitions for events added

--- components/navigation/EventsStackNavigator.jsx
+++ components/navigation/EventsStackNavigator.jsx
@@ -3,13 +3,17 @@ import { createStackNavigator } from "@react-navigation/stack";
 import EventListScreen from "../../screens/Central/EventList";
 import EventDetailsScreen from "../../screens/Central/EventDetails";
 
-
+const fade = ({ current }) => ({
+    cardStyle: {
+      opacity: current.progress,
+    },
+});
 
 const Stack = createStackNavigator();
 
 export default function EventsStackNavigator() {
     return (
-        <Stack.Navigator initialRouteName="Events" screenOptions={{ headerShown: false }}>
+        <Stack.Navigator initialRouteName="Events" screenOptions={{ headerShown: false, cardStyleInterpolator: fade}}>
             <Stack.Screen name="Events" component={EventListScreen} />
 
             <Stack.Screen name="EventDetailsScreen" component={EventDetailsScreen}/>
