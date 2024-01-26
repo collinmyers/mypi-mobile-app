80f5c9b3f40a6a61c9e1912719bbeb118855afc5
Collin Myers <collin_myers@outlook.com>
2024-01-25 23:54:46 -0500

    Updated events list page to be able to render more than 25 queries at a time

--- screens/Central/EventList.jsx
+++ screens/Central/EventList.jsx
@@ -3,13 +3,15 @@ import { useFocusEffect } from "@react-navigation/native";
 
 import { ScrollView, SafeAreaView, Pressable, Image } from "react-native";
 import { Card, Text } from "react-native-paper";
-import { Databases, Client, Storage } from "appwrite";
+import { Databases, Client, Storage, Query } from "appwrite";
 import { useNavigation } from "@react-navigation/native";
 import HomeStyle from "../../styling/HomeStyle";
 
 export default function EventListScreen() {
     const navigation = useNavigation();
     const [data, setData] = useState([]);
+
+    const PAGE_SIZE = 25;
 
     useFocusEffect(
         React.useCallback(() => {
@@ -41,55 +43,69 @@ export default function EventListScreen() {
             const database = new Databases(client);
             const storage = new Storage(client);
 
-            const response = await database.listDocuments(
-                "653ae4b2740b9f0a5139", // DB ID
-                "655280f07e30eb37c8e8" // Collection ID
-            );
+            let offset = 0;
+            let allData = [];
 
-
-            // Build a new array with updated data
-            const newData = response.documents.map((document, index) => {
-                const EventListDescription = document.EventListDescription;
-                const EventName = document.Name;
-                const EventDateTime = document.DateTime;
-                const EventDetailsDescription = document.EventDetailsDescription;
-                const EventLatitude = document.Latitude;
-                const EventLongitude = document.Longitude;
-
-                const EventImage = storage.getFileView(
-                    "653ae4d2b3fcc68c10bf", // BucketID
-                    document.FileID // File ID
-                ).toString();
-
-                return (
-                    <Pressable
-                        key={index}
-                        onPress={() =>
-                            navigation.navigate("EventDetailsScreen", {
-                                EventImage: EventImage,
-                                EventName: EventName,
-                                EventDateTime: EventDateTime,
-                                EventDetailsDescription: EventDetailsDescription,
-                                EventListDescription: EventListDescription,
-                                EventLatitude: EventLatitude,
-                                EventLongitude: EventLongitude
-                            })
-                        }
-                    >
-                        <Card style={HomeStyle.eventCard}>
-                            <Card.Content style={HomeStyle.eventCardContent}>
-                                <Image source={{ uri: EventImage }} style={HomeStyle.eventListImage} />
-                                <Text style={HomeStyle.eventListTitle}>{EventName}</Text>
-                                <Text style={HomeStyle.eventListDateTime}>{EventDateTime}</Text>
-                                <Text style={HomeStyle.eventListDescription}>{EventListDescription}</Text>
-
-                            </Card.Content>
-                        </Card>
-                    </Pressable>
+            const fetchPage = async (offset) => {
+                const response = await database.listDocuments(
+                    "653ae4b2740b9f0a5139", // DB ID
+                    "655280f07e30eb37c8e8", // Collection ID
+                    [
+                        Query.limit(PAGE_SIZE),
+                        Query.offset(offset)
+                    ]
                 );
-            });
-            // Update state with the new array
-            setData(newData);
+
+                const newData = response.documents.map((document, index) => {
+                    const EventListDescription = document.EventListDescription;
+                    const EventName = document.Name;
+                    const EventDateTime = document.DateTime;
+                    const EventDetailsDescription = document.EventDetailsDescription;
+                    const EventLatitude = document.Latitude;
+                    const EventLongitude = document.Longitude;
+
+                    const EventImage = storage.getFileView(
+                        "653ae4d2b3fcc68c10bf", // BucketID
+                        document.FileID // File ID
+                    ).toString();
+
+                    return (
+                        <Pressable
+                            key={`${offset}_${index}`}
+                            onPress={() =>
+                                navigation.navigate("EventDetailsScreen", {
+                                    EventImage: EventImage,
+                                    EventName: EventName,
+                                    EventDateTime: EventDateTime,
+                                    EventDetailsDescription: EventDetailsDescription,
+                                    EventListDescription: EventListDescription,
+                                    EventLatitude: EventLatitude,
+                                    EventLongitude: EventLongitude
+                                })
+                            }
+                        >
+                            <Card style={HomeStyle.eventCard}>
+                                <Card.Content style={HomeStyle.eventCardContent}>
+                                    <Image source={{ uri: EventImage }} style={HomeStyle.eventListImage} />
+                                    <Text style={HomeStyle.eventListTitle}>{EventName}</Text>
+                                    <Text style={HomeStyle.eventListDateTime}>{EventDateTime}</Text>
+                                    <Text style={HomeStyle.eventListDescription}>{EventListDescription}</Text>
+                                </Card.Content>
+                            </Card>
+                        </Pressable>
+                    );
+                });
+
+                allData = [...allData, ...newData];
+
+                if (response.documents.length === PAGE_SIZE) {
+                    await fetchPage(offset + PAGE_SIZE); // Fetch next page
+                }
+            };
+
+            await fetchPage(offset); // Start fetching from the initial offset
+
+            setData(allData);
         } catch (error) {
             console.error(error);
         }
