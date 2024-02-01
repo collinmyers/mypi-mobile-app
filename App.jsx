525a15dd55d65c5c11d76b549c3d5c1c482219a5
Collin Myers <collin_myers@outlook.com>
2024-01-31 22:11:14 -0500

    Removed expo statusbar, alternate solution to fix issue

--- App.jsx
+++ App.jsx
@@ -2,15 +2,12 @@ import React from "react";
 import DrawerNavigator from "./components/navigation/DrawerNavigator";
 import { setupURLPolyfill } from "react-native-url-polyfill";
 import storage from "local-storage-fallback";
-import { StatusBar } from "expo-status-bar";
 
 export default function App() {
     setupURLPolyfill();
     if (!("localStorage" in window)) window.localStorage = storage;
 
     return (
-        <DrawerNavigator >
-            <StatusBar />
-        </DrawerNavigator>
+        <DrawerNavigator />
     );
 }
