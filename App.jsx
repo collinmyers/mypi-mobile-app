1fe0f81f42af4b7fe4d2bec4ab0752212cfb9aba
Collin Myers <collin_myers@outlook.com>
2024-02-01 19:57:08 -0500

    Added expo statusbar back, apparently idk what I am doing with this statusbar at this point...

--- App.jsx
+++ App.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import DrawerNavigator from "./components/navigation/DrawerNavigator";
 import { setupURLPolyfill } from "react-native-url-polyfill";
 import storage from "local-storage-fallback";
+import { StatusBar } from "expo-status-bar";
+
 
 export default function App() {
     setupURLPolyfill();
     if (!("localStorage" in window)) window.localStorage = storage;
 
     return (
-        <DrawerNavigator />
+        <DrawerNavigator>
+            <StatusBar style="dark" />
+        </DrawerNavigator>
     );
 }
