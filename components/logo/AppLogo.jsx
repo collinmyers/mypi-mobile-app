98bae475bbf902f4f631d053b1e460f107f8d2d2
Collin Myers <collin_myers@outlook.com>
2024-02-03 04:11:04 -0500

    Reverted image for app

--- components/logo/AppLogo.jsx
+++ components/logo/AppLogo.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Image } from "react-native";
 import Logo from "../../assets/myPILogo.png";
-import LogoAlt from "../../assets/myPILogoAltOutline.png";
+import LogoAlt from "../../assets/myPILogoAlt.png";
 import { useAltUI } from "../../utils/colors/appColors";
 import PropTypes from "prop-types";
 
