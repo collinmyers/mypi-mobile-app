ed0aaf370970835dcdcd17badd8a6cf09745c99e
Collin Myers <collin_myers@outlook.com>
2024-02-18 21:46:58 -0500

    Set up event list screen to be ready to implement offline access

--- screens/Central/EventList.jsx
+++ screens/Central/EventList.jsx
@@ -10,7 +10,7 @@ import { subscribeToRealTimeUpdates } from "../../utils/Config/appwriteConfig";
 
 export default function EventListScreen() {
     const navigation = useNavigation();
-    const [data, setData] = useState([]);
+    const [eventData, setEventData] = useState([]);
 
     const PAGE_SIZE = 25;
 
@@ -19,12 +19,65 @@ export default function EventListScreen() {
     useFocusEffect(React.useCallback(() => {
         // Function to handle real-time updates
         const handleSubscription = () => {
-            getEvents();
+            fetchData();
         };
 
         const unsubscribe = subscribeToRealTimeUpdates(handleSubscription, EVENTS_COLLECTION_ID);
 
-        getEvents();
+        const fetchData = async () => {
+            try {
+                let offset = 0;
+                let allEvents = [];
+
+                const response = await database.listDocuments(
+                    DATABASE_ID,
+                    EVENTS_COLLECTION_ID,
+                    [Query.limit(PAGE_SIZE), Query.offset(offset)]
+                );
+
+                while (response.documents.length > 0) {
+                    allEvents = [...allEvents, ...response.documents];
+                    offset += PAGE_SIZE;
+                    const nextResponse = await database.listDocuments(
+                        DATABASE_ID,
+                        EVENTS_COLLECTION_ID,
+                        [Query.limit(PAGE_SIZE), Query.offset(offset)]
+                    );
+                    response.documents = nextResponse.documents;
+                }
+
+                // Fetch images for each event concurrently
+                const eventsWithImages = await Promise.all(allEvents.map(async (event) => {
+                    const EventListDescription = event.EventListDescription;
+                    const EventName = event.Name;
+                    const EventDateTime = event.DateTime;
+                    const EventDetailsDescription = event.EventDetailsDescription;
+                    const EventLatitude = event.Latitude;
+                    const EventLongitude = event.Longitude;
+
+                    const imageResponse = await storage.getFileView(FILE_BUCKET_ID, event.FileID);
+                    const EventImage = imageResponse.toString(); // Assuming imageResponse is a URL
+
+                    return {
+                        EventName,
+                        EventDateTime,
+                        EventDetailsDescription,
+                        EventListDescription,
+                        EventLatitude,
+                        EventLongitude,
+                        EventImage
+                    };
+                }));
+
+                setEventData(eventsWithImages);
+                // await saveDataToFile(allPoints); // Save fetched data to file
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+
+        fetchData();
 
         // Cleanup function
         return () => {
@@ -32,81 +85,51 @@ export default function EventListScreen() {
         };
     }, []));
 
-    const getEvents = async () => {
-        try {
+    const renderEvents = () => {
+        return eventData.map((event, index) => {
 
-            let offset = 0;
-            let allData = [];
 
-            const fetchPage = async (offset) => {
-                const response = await database.listDocuments(
-                    DATABASE_ID,
-                    EVENTS_COLLECTION_ID,
-                    [
-                        Query.limit(PAGE_SIZE),
-                        Query.offset(offset)
-                    ]
-                );
+            const EventName = event.EventName;
+            const EventDateTime = event.EventDateTime;
+            const EventListDescription = event.EventListDescription;
+            const EventDetailsDescription = event.EventDetailsDescription;
+            const EventLatitude = event.EventLatitude;
+            const EventLongitude = event.EventLongitude;
+            const EventImage = event.EventImage;
 
-                const newData = response.documents.map((document, index) => {
-                    const EventListDescription = document.EventListDescription;
-                    const EventName = document.Name;
-                    const EventDateTime = document.DateTime;
-                    const EventDetailsDescription = document.EventDetailsDescription;
-                    const EventLatitude = document.Latitude;
-                    const EventLongitude = document.Longitude;
-
-                    const EventImage = storage.getFileView(
-                        FILE_BUCKET_ID, // BucketID
-                        document.FileID // File ID
-                    ).toString();
-
-                    return (
-                        <Pressable
-                            key={`${offset}_${index}`}
-                            onPress={() =>
-                                navigation.navigate("EventDetailsScreen", {
-                                    EventImage: EventImage,
-                                    EventName: EventName,
-                                    EventDateTime: EventDateTime,
-                                    EventDetailsDescription: EventDetailsDescription,
-                                    EventListDescription: EventListDescription,
-                                    EventLatitude: EventLatitude,
-                                    EventLongitude: EventLongitude
-                                })
-                            }
-                        >
-                            <Card style={HomeStyle.eventCard}>
-                                <Card.Content style={HomeStyle.eventCardContent}>
-                                    <Image source={{ uri: EventImage }} style={HomeStyle.eventListImage} />
-                                    <Text style={HomeStyle.eventListTitle}>{EventName}</Text>
-                                    <Text style={HomeStyle.eventListDateTime}>{EventDateTime}</Text>
-                                    <Text style={HomeStyle.eventListDescription}>{EventListDescription}</Text>
-                                </Card.Content>
-                            </Card>
-                        </Pressable>
-                    );
-                });
-
-                allData = [...allData, ...newData];
-
-                if (response.documents.length === PAGE_SIZE) {
-                    await fetchPage(offset + PAGE_SIZE); // Fetch next page
-                }
-            };
-
-            await fetchPage(offset); // Start fetching from the initial offset
-
-            setData(allData);
-        } catch (error) {
-            console.error(error);
-        }
+            return (
+                <Pressable
+                    key={`${index}_${EventName}`}
+                    onPress={() =>
+                        navigation.navigate("EventDetailsScreen", {
+                            EventImage: EventImage,
+                            EventName: EventName,
+                            EventDateTime: EventDateTime,
+                            EventDetailsDescription: EventDetailsDescription,
+                            EventListDescription: EventListDescription,
+                            EventLatitude: EventLatitude,
+                            EventLongitude: EventLongitude
+                        })
+                    }
+                >
+                    <Card style={HomeStyle.eventCard}>
+                        <Card.Content style={HomeStyle.eventCardContent}>
+                            <Image source={{ uri: EventImage }} style={HomeStyle.eventListImage} />
+                            <Text style={HomeStyle.eventListTitle}>{EventName}</Text>
+                            <Text style={HomeStyle.eventListDateTime}>{EventDateTime}</Text>
+                            <Text style={HomeStyle.eventListDescription}>{EventListDescription}</Text>
+                        </Card.Content>
+                    </Card>
+                </Pressable>
+            );
+        });
     };
+
 
     return (
         <SafeAreaView style={HomeStyle.eventContainer}>
             <ScrollView contentContainerStyle={HomeStyle.scrollableView} showsVerticalScrollIndicator={false}>
-                {data}
+                {renderEvents()}
             </ScrollView>
         </SafeAreaView>
     );
