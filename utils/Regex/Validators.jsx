f14ab0a3c11f30238b63bd639c5c63c699a8faec
Collin Myers <collin_myers@outlook.com>
2024-03-01 12:18:17 -0500

    Updated validators to be more relaxed and for updated error handling

--- utils/Regex/Validators.jsx
+++ utils/Regex/Validators.jsx
@@ -1,28 +1,23 @@
-// validators.js
-export const validateName = (name, setName) => {
-    const nameRegex = /^[a-zA-Z]+$/;
-
-    if (!nameRegex.test(name)) {
-        setName("");
-    }
+// validators.jsx
+export const validateName = (name) => {
+    return name.length > 0;  // True if name has at least one character
 };
 
-export const validateEmail = (email, setEmail) => {
+export const validateEmail = (email) => {
     const emailRegex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-
-    if (!emailRegex.test(email)) {
-        setEmail("");
-    }
+    return emailRegex.test(email);
 };
 
-export const validatePassword = (password, confirmPassword, setPassword, setConfirmPassword) => {
-    
+export const validatePassword = (password, confirmPassword) => {
+
     if (password.length < 8) {
-        setPassword("");
+        return "Password must be at least 8 characters.";
     }
-    
+
     if (password !== confirmPassword) {
-        setPassword("");
-        setConfirmPassword("");
+        return "Passwords do not match.";
     }
+
+    return ""; // Return an empty string if validation passes
 };
+
