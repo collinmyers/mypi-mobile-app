b1e7736d214edd863cfa18991d6075fc7bd5a923
Collin Myers <collin_myers@outlook.com>
2024-03-19 14:58:50 -0400

    Updated notification logic

--- App.jsx
+++ App.jsx
@@ -6,7 +6,6 @@ import { Platform } from "react-native";
 import * as Notifications from "expo-notifications";
 import { account, database, DATABASE_ID, USER_NOTIFICATION_TOKENS } from "./utils/Config/appwriteConfig";
 import { ID } from "appwrite";
-import * as FileSystem from "expo-file-system";
 
 export default function App() {
     setupURLPolyfill();
@@ -46,19 +45,6 @@ export default function App() {
                 // If granted, get the token and create a document in appwrite
                 token = (await Notifications.getExpoPushTokenAsync({ projectID: "myPI" })).data;
 
-                // Check if expoToken is stored. If it is, do nothing. Else create a new doc...
-                const { exists } = await FileSystem.getInfoAsync(FileSystem.documentDirectory + "expoPushToken.json");
-
-                if (exists) {
-                    const fileUri = FileSystem.documentDirectory + "expoPushToken.json";
-                    const fileContents = await FileSystem.readAsStringAsync(fileUri);
-                    const data = JSON.parse(fileContents);
-                    if (data === token) {
-                        console.log("Notification Token already stored");
-                        return;
-                    }
-                }
-
                 // Create doc for user's push token. This will run if the expo token is not stored or doesn't match.
                 const createTokenDoc = database.createDocument(
                     DATABASE_ID,
@@ -71,25 +57,12 @@ export default function App() {
                 }, function (error) {
                     console.log(error);
                 });
-
-                writeFile(token);
             } catch (error) {
                 console.error("Error: ", error);
             }
         };
-
         getPermissions();
     }, []);
-
-    const writeFile = async (data) => {
-        try {
-            const fileUri = FileSystem.documentDirectory + "expoPushToken.json";
-            await FileSystem.writeAsStringAsync(fileUri, JSON.stringify(data));
-            console.log("Data saved to file:", fileUri);
-        } catch (error) {
-            console.error("Error saving data to file: ", error);
-        }
-    };
 
 
     return (
