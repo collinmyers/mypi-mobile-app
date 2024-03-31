e6fa79a92b6a4cc14a715069a92c80f421822bcf
Collin Myers <collin_myers@outlook.com>
2024-03-30 22:04:20 -0400

    Removed unused packages, added prop validation

--- screens/Sidebar/Donation.jsx
+++ screens/Sidebar/Donation.jsx
@@ -4,86 +4,97 @@ import { View, ScrollView, RefreshControl } from "react-native";
 import { ActivityIndicator, Text } from "react-native-paper";
 import HomeStyle from "../../styling/HomeStyle";
 import { appPrimaryColor, appQuarternaryColor, appSecondaryColor } from "../../utils/colors/appColors";
+import PropTypes from "prop-types";
 
 const WebViewComponent = ({ uri, onError }) => {
-  const webViewRef = useRef(null);
-  const [refreshing, setRefreshing] = useState(false);
+    const webViewRef = useRef(null);
+    const [refreshing, setRefreshing] = useState(false);
 
-  const onRefresh = useCallback(() => {
-    setRefreshing(true);
+    const onRefresh = useCallback(() => {
+        setRefreshing(true);
 
-    // Reload the WebView if it's mounted
-    if (webViewRef.current) {
-      webViewRef.current.reload();
-    }
-  }, []);
+        // Reload the WebView if it's mounted
+        if (webViewRef.current) {
+            webViewRef.current.reload();
+        }
+    }, []);
 
-  useEffect(() => {
-    if (!refreshing) {
-      setRefreshing(false);
-    }
-  }, [refreshing]);
+    useEffect(() => {
+        if (!refreshing) {
+            setRefreshing(false);
+        }
+    }, [refreshing]);
 
-  return (
-    <WebView
-      ref={webViewRef}
-      source={{ uri }}
-      style={HomeStyle.donationsWebView}
-      onError={onError}
-      renderLoading={() => (
-        <View style={HomeStyle.activityIndicatorContainer}>
-          <ActivityIndicator animating={true} color={appSecondaryColor} size="large" />
-        </View>
-      )}
-      refreshControl={
-        <RefreshControl
-          style={{ backgroundColor: appQuarternaryColor }}
-          refreshing={refreshing}
-          onRefresh={onRefresh}
-          colors={[appPrimaryColor]}
-          tintColor={appPrimaryColor}
+    return (
+        <WebView
+            ref={webViewRef}
+            source={{ uri }}
+            style={HomeStyle.donationsWebView}
+            onError={onError}
+            renderLoading={() => (
+                <View style={HomeStyle.activityIndicatorContainer}>
+                    <ActivityIndicator animating={true} color={appSecondaryColor} size="large" />
+                </View>
+            )}
+            refreshControl={
+                <RefreshControl
+                    style={{ backgroundColor: appQuarternaryColor }}
+                    refreshing={refreshing}
+                    onRefresh={onRefresh}
+                    colors={[appPrimaryColor]}
+                    tintColor={appPrimaryColor}
+                />
+            }
         />
-      }
-    />
-  );
+    );
+};
+
+WebViewComponent.propTypes = {
+    uri: PropTypes.string.isRequired,
+    onError: PropTypes.func.isRequired,
 };
 
 const ErrorScreen = ({ onRefresh, refreshing }) => {
-  return (
-    <ScrollView
-      showsVerticalScrollIndicator={false}
-      contentContainerStyle={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: appPrimaryColor }}
-      refreshControl={
-        <RefreshControl
-          style={{ backgroundColor: appQuarternaryColor }}
-          refreshing={refreshing}
-          onRefresh={onRefresh}
-          colors={[appPrimaryColor]}
-          tintColor={appPrimaryColor}
-        />
-      }
-    >
-      <Text style={{ color: appSecondaryColor, fontWeight: "500", fontSize: 18 }}>No internet connection. Swipe down to refresh.</Text>
-    </ScrollView>
-  );
+    return (
+        <ScrollView
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: appPrimaryColor }}
+            refreshControl={
+                <RefreshControl
+                    style={{ backgroundColor: appQuarternaryColor }}
+                    refreshing={refreshing}
+                    onRefresh={onRefresh}
+                    colors={[appPrimaryColor]}
+                    tintColor={appPrimaryColor}
+                />
+            }
+        >
+            <Text style={{ color: appSecondaryColor, fontWeight: "500", fontSize: 18 }}>No internet connection. Swipe down to refresh.</Text>
+        </ScrollView>
+    );
+};
+
+ErrorScreen.propTypes = {
+    onRefresh: PropTypes.func.isRequired,
+    refreshing: PropTypes.bool.isRequired,
 };
 
 export default function DonationsScreen() {
-  const [isErrored, setIsErrored] = useState(false);
-  const [refreshing, setRefreshing] = useState(false);
+    const [isErrored, setIsErrored] = useState(false);
+    const [refreshing, setRefreshing] = useState(false);
 
-  return (
-    <>
-      {!isErrored ? (
-        <WebViewComponent
-          uri={process.env.EXPO_PUBLIC_DONATION_PROVIDER_LINK}
-          onError={() => setIsErrored(true)}
-          refreshing={refreshing}
-          setRefreshing={setRefreshing}
-        />
-      ) : (
-        <ErrorScreen onRefresh={() => setIsErrored(false)} refreshing={refreshing} />
-      )}
-    </>
-  );
+    return (
+        <>
+            {!isErrored ? (
+                <WebViewComponent
+                    uri={process.env.EXPO_PUBLIC_DONATION_PROVIDER_LINK}
+                    onError={() => setIsErrored(true)}
+                    refreshing={refreshing}
+                    setRefreshing={setRefreshing}
+                />
+            ) : (
+                <ErrorScreen onRefresh={() => setIsErrored(false)} refreshing={refreshing} />
+            )}
+        </>
+    );
 }
