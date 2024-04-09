4253879220096256d0b93fa8a80b688e6dabcd12
Collin Myers <collin_myers@outlook.com>
2024-04-09 15:57:32 -0700

    Updated to remove view

--- components/Keyboard/KeyboardAvoidingComponent.jsx
+++ components/Keyboard/KeyboardAvoidingComponent.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { View } from "react-native";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 const KeyboardAvoidingComponent = ({ children, style }) => {
     return (
         <KeyboardAwareScrollView style={style} scrollEnabled={false} contentContainerStyle={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-            <View style={{ width: "100%" }}>
-                {children}
-            </View>
+            {children}
         </KeyboardAwareScrollView>
     );
 };
