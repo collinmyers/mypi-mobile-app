766a6e44989b1b0705c04bed8cd2e40b575e3432
Collin Myers <collin_myers@outlook.com>
2024-04-09 16:59:56 -0700

    Fixed keyboard avoiding view with custom solution to fix jerky movement for rn keyboard avoiding view. Removed unused packages for kav

--- components/Keyboard/KeyboardAvoidingComponent.jsx
+++ components/Keyboard/KeyboardAvoidingComponent.jsx
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
-import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { ScrollView, KeyboardAvoidingView, Platform, Keyboard } from "react-native";
+
+const useKeyboardVisible = () => {
+    const [isKeyboardVisible, setKeyboardVisible] = useState(false);
+
+    useEffect(() => {
+        const keyboardDidShowListener = Keyboard.addListener("keyboardDidShow", () => { setKeyboardVisible(true); },);
+
+        const keyboardDidHideListener = Keyboard.addListener("keyboardDidHide", () => { setKeyboardVisible(false); },);
+
+        return () => {
+            keyboardDidHideListener.remove();
+            keyboardDidShowListener.remove();
+        };
+    }, []);
+
+    return isKeyboardVisible;
+};
 
 const KeyboardAvoidingComponent = ({ children, style }) => {
+    const isKeyboardVisible = useKeyboardVisible();
+
     return (
-        <KeyboardAwareScrollView style={style} scrollEnabled={false} contentContainerStyle={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-            {children}
-        </KeyboardAwareScrollView>
+        <KeyboardAvoidingView
+            contentContainerStyle={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            style={style}
+        >
+            <ScrollView scrollEnabled={isKeyboardVisible} showsVerticalScrollIndicator={false}>
+                {children}
+            </ScrollView>
+        </KeyboardAvoidingView>
     );
 };
 
